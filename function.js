93e85a71dc32152e4d1f2504483e8848459dd0f5
Pranay K R <2005pranaykr@gmail.com>
2024-08-18 16:47:40 +0530

    added function.js

--- function.js
+++ function.js
@@ -17,6 +17,7 @@ function addNumbers(num1,num2){
 const result = addNumbers(4,3);
 console.log(`Result: ${result}`); //undefined is returned since the function does not return anything.
 
+
 function loginUserMessage(username="default"){ //default value is set here
     if(username===undefined){
         console.log("Please enter a valid username");
@@ -25,4 +26,39 @@ function loginUserMessage(username="default"){ //default value is set here
     }
     return `${username} just loggen in`; 
 }
+
 console.log(loginUserMessage());  // if u dont pass anything gonna return undefined 
+
+function shoppingCart(val1,val2,...value){  //if there exists more parameters then lets see what happens. 
+    return value; //does return the value lets if we have more than one value then..          
+}
+
+console.log(shoppingCart(10,1000,10000,200)); //entries are returned as an array . 
+//this down shows us only the last two values are being returned and the other two r not even being considered even though the parameters for them are present. 
+
+const user = {
+    userName: 'Pranay', 
+    days: 100,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is : ${anyObject.userName} and days is : ${anyObject.days}`);
+}
+
+//can pass in this way or might as well pass a object directly.
+// handleObject(user);
+handleObject({
+    userName: 'John', 
+    days: 1000,
+})
+
+const array = [100,200,300,400.500]; 
+
+function returnSecondValue(getArray){
+    return getArray[1];  //returns the second value in the array. accessing array via index values 
+}
+//can pass again via an array declared previously or do it directly 
+console.log(returnSecondValue(array));
+console.log(returnSecondValue([200,300,500.600]));
+
+
