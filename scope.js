374d15627c26b2eb213763aa62e4600fc5160004
Pranay K R <2005pranaykr@gmail.com>
2024-08-25 13:06:39 +0530

    finished scope.js

--- scope.js
+++ scope.js
@@ -10,3 +10,40 @@ if(true){
 }
 console.log(a)
 
+//nested scope
+
+function one(){
+    const userName= 'Pranay'
+
+    function two(){
+        const website = 'github'
+        console.log(userName); // it will work as userName is defined in parent scope
+        console.log(website)
+    }
+    // console.log(website); // it will give error as website is not defined
+    two();
+    
+}
+one();
+
+if(true){
+    const name = 'Pranay '
+    if(name=='Pranay '){
+        const website = 'github'
+      console.log(name+website);
+    }
+    // console.log(website); // it will give error as website is not defined
+    
+}
+// console.log(name); // it will give error as name is not defined
+addOne(5); //can use function before defining it
+function addOne(value){
+    return value+1;
+}
+
+//addtwo(5); //can't use function before defining it    
+const addtwo = function(value){
+    return value+2;
+}
+
+console.log(addtwo(5));
